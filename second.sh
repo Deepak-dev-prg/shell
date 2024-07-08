fd60d6e4b0285c0445bdedf586c8a8fd5e141903
Deepak <deepakdeepu7670@gmail.com>
2024-07-08 18:29:55 +0530

    update

--- second.sh
+++ second.sh
@@ -1,2 +1,9 @@
-this is my seocnd shell
-this is my seocnd shell
+#!/bin/bash 
+
+PERSON1=deepak
+PERSON2=deepu 
+
+echo $PERSON1 "hi this h,vhsadhdbhbc"
+echo $PERSON2 "hi sdlbsdgcsdhb"
+echo $PERSON1 "hi this h,vhsadhdbhbahsdgchdvssahdkvc"
+echo $PERSON2 "hi sdlbsdgcsdhbjs,bdhcsdhysdchd"
